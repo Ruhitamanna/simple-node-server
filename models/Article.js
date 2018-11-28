9231d76489e07883107d64d69546436e5b53f5ba
Ruhi Tamanna Alam <ruhialamsithy@gmail.com>
2018-11-28 03:26:57 +0000

    classwork 9&10

--- models/Article.js
+++ models/Article.js
@@ -1,16 +1,19 @@
 var mongoose = require("mongoose");
-
 var Schema = mongoose.Schema;
-
 var articleSchema = new Schema({
   title: {
     type: String,
-    required: "Title required"
+    required: "Title Required"
   },
   content: {
     type: String
+  },
+  reading_time:{
+    type: Number,
+    required: "Reading time required"
   }
 });
 
-var Article = mongoose.model('Article', articleSchema)
+var Article = mongoose.model('Article', articleSchema);
+
 module.exports = Article;
